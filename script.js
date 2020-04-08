aea96f5dfeb7c6e8ba5eb0e6fe089555f2b186bd
erickarugu32 <erickarugu32@gmail.com>
2020-04-08 23:18:05 +0300

    Added footer and more forecast data

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ weatherConditions.onload = function() {
     if (weatherConditions.status === 200){
         cObj = JSON.parse(weatherConditions.responseText); 
         console.log(cObj);
-        var tempCel = (parseInt(cObj.main.temp) - 32) * (5/6);
-        document.getElementById('location').innerHTML = cObj.name;
-        document.getElementById('weather').innerHTML = cObj.weather[0].description;
-        document.getElementById('temperature').innerHTML = tempCel+ " <span>&#176;</span>C";
+        //var tempCel = ((parseInt(fObj.list[0].main.temp_min) - 32) * (5/6)).toFixed(2);
+        document.getElementById('location').innerHTML = cObj.name + ", "+cObj.sys.country;
+        document.getElementById('weather').innerHTML = cObj.weather[0].main+": "+cObj.weather[0].description;
+        document.getElementById('temperature').innerHTML = ((parseInt(cObj.main.temp) - 32) * (5/6)).toFixed(2)+ " <span>&#176;</span>C";
         document.getElementById('desc').innerHTML = "Wind Speed:  "+cObj.wind.speed + "Km/h";
 
 
@@ -36,39 +36,49 @@ if (weatherForecast.status === 200){
     
     //day 1
     var raw_date = fObj.list[0].dt_txt;
-    raw_date = raw_date.substring(5,11);
+
+    raw_date = raw_date.substring(0,11);
+    raw_date = new Date(raw_date);
+    raw_date = raw_date.toDateString();
     document.getElementById('c1date').innerHTML = raw_date;
 
     var iconcode = fObj.list[0].weather[0].icon;
     var icon_path = "https://openweathermap.org/img/w/" +iconcode+ ".png";
     document.getElementById('c1img').src = icon_path;
 
-    document.getElementById('c1tmp_min').innerHTML = "Min: "+fObj.list[0].main.temp_min +" &deg;F";
-    document.getElementById('c1tmp_max').innerHTML = "Max: "+fObj.list[0].main.temp_max +" &deg;F";
+    document.getElementById('c1tmp_min').innerHTML = "Min: "+((parseInt(fObj.list[0].main.temp_min) - 32) * (5/6)).toFixed(2) +" &deg;C";
+    document.getElementById('c1tmp_max').innerHTML = "Max: "+((parseInt(fObj.list[0].main.temp_max) - 32) * (5/6)).toFixed(2) +" &deg;C";
+    document.getElementById('c1weather').innerHTML = fObj.list[0].weather[0].main+": "+fObj.list[0].weather[0].description;
 
     //day 2
     var raw_date = fObj.list[8].dt_txt;
-    raw_date = raw_date.substring(5,11);
+    raw_date = new Date(raw_date);
+    raw_date = raw_date.toDateString();
+    //raw_date = raw_date.substring(5,11);
     document.getElementById('c2date').innerHTML = raw_date;
 
     var iconcode = fObj.list[8].weather[0].icon;
     var icon_path = "https://openweathermap.org/img/w/" +iconcode+ ".png";
     document.getElementById('c2img').src = icon_path;
 
-    document.getElementById('c2tmp_min').innerHTML = "Min: " +fObj.list[8].main.temp_min +" &deg;F";
-    document.getElementById('c2tmp_max').innerHTML = "Max: "+fObj.list[8].main.temp_max +" &deg;F";
+    document.getElementById('c2tmp_min').innerHTML = "Min: " +((parseInt(fObj.list[8].main.temp_min) - 32) * (5/6)).toFixed(2) +" &deg;C";
+    document.getElementById('c2tmp_max').innerHTML = "Max: "+((parseInt(fObj.list[8].main.temp_max) - 32) * (5/6)).toFixed(2) +" &deg;C";
+    document.getElementById('c2weather').innerHTML = fObj.list[8].weather[0].main+": "+fObj.list[8].weather[0].description;
 
      //day 3
      var raw_date = fObj.list[16].dt_txt;
-     raw_date = raw_date.substring(5,11);
+     raw_date = new Date(raw_date);
+    raw_date = raw_date.toDateString();
+     //raw_date = raw_date.substring(5,11);
      document.getElementById('c3date').innerHTML = raw_date;
  
      var iconcode = fObj.list[16].weather[0].icon;
      var icon_path = "https://openweathermap.org/img/w/" +iconcode+ ".png";
      document.getElementById('c3img').src = icon_path;
  
-     document.getElementById('c3tmp_min').innerHTML = fObj.list[16].main.temp_min +" &deg;";
-     document.getElementById('c3tmp_max').innerHTML = fObj.list[16].main.temp_max +" &deg;";
+     document.getElementById('c3tmp_min').innerHTML = "Min: "+((parseInt(fObj.list[16].main.temp_min) - 32) * (5/6)).toFixed(2) +" &deg;C";
+     document.getElementById('c3tmp_max').innerHTML = "Max: "+((parseInt(fObj.list[16].main.temp_max) - 32) * (5/6)).toFixed(2); +" &deg;C";
+     document.getElementById('c3weather').innerHTML = fObj.list[16].weather[0].main+": "+fObj.list[16].weather[0].description;
 	
 } //end if
 }; //end function
